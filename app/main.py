7946b5f92a56d26fc2d787977b94e1fc48d84253
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-10-30 20:26:11 +0100

    refactor: use port from settings for fastapi app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
+from config import settings
+
 
 app = FastAPI()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=settings.port)
