6d66d9ee22a9672ac1ff591d52e4b70f8c933ec7
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-10-30 22:48:46 +0100

    feat: add db support

--- app/config.py
+++ app/config.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     port: int
 
+    database_username: str
+    database_password: str
+    database_name: str
+    database_host: str
+    database_port: int
+
     model_config = SettingsConfigDict(
         env_file=".env",
     )
