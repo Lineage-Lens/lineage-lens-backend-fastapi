e13ab0e3d79ae40f6c2728b0242fcfe1fde858cc
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-22 03:47:54 +0100

    chore: add allowed origins to Settings

--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     port: int
 
+    allowed_origins: list[str]
+
     database_username: str
     database_password: str
     database_name: str
