7f50593eeac2ca52f6f8bcf92d2a14d86df36996
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-11-23 23:21:28 +0100

    feat: add RelationshipRepository to di

--- app/di.py
+++ app/di.py
@@ -1,6 +1,8 @@
 from db import get_session
 from crud.person import PersonRepository
+from crud.relationship import RelationshipRepository
 
 
 session = next(get_session())
 person_repository = PersonRepository(session)
+relationship_repository = RelationshipRepository(session)
